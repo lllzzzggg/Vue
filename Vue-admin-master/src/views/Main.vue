832576d9c1b5ec0400983cbb8c21e22432140c26
lzg <1649973704@qq.com>
2018-10-10 10:56:30 +0800

    add

---
+++ Vue-admin-master/src/views/Main.vue
@@ -0,0 +1,45 @@
+<template>
+	<section>
+		<p>{{ storeMessage }}</p>
+		<children 
+			v-for="post in posts"
+			v-bind:key="post.id"
+			v-bind:title="post.title"
+			v-on:large="postFontSize += 0.1"
+			:style="{fontSize: postFontSize + 'em'}"
+		></children>
+	</section>
+</template>
+
+<script>
+import child from './nav1/main_children'
+	export default {
+		data(){
+			return {
+				posts:[
+					{ id: 1, title: 'My journey with Vue' },
+					{ id: 2, title: 'Blogging with Vue' },
+					{ id: 3, title: 'Why Vue is so fun' }
+				],
+				postFontSize: 1,
+				message:'dsds'
+			}
+		},
+		components:{
+			"children":child
+		},
+		computed:{
+			storeMessage(){
+				return this.$store.state.msg
+			},
+			showMsg(msg){
+				this.message = msg
+			}
+		}
+	}
+
+</script>
+
+<style scoped>
+
+</style>
