d6dd58723d4ac423e85a080ac45fbb04de78893c
lzg <1649973704@qq.com>
2018-10-11 09:48:12 +0800

    change vue-demo

--- Vue-admin-master/src/views/nav3/Page6.vue
+++ Vue-admin-master/src/views/nav3/Page6.vue
@@ -1,4 +1,15 @@
 <template>
-	<section>page6...
+	<section>
+		page6...
+		<Test></Test>
 	</section>
 </template>
+<script>
+	import Test from "./test.vue"
+	export default {
+		name: 'app',
+		components: {
+			Test
+		}
+	}
+</script>
---
+++ Vue-admin-master/src/views/nav3/test.vue
@@ -0,0 +1,3 @@
+<template>
+	<h1>1234</h1>
+</template>
